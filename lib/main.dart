3c72cecf9d5461d33c1dff52aaab685db2583947
mohammed <mohammed.abdelfttah0@gmail.com>
2023-07-14 16:05:22 +0300

    intial project

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const FactoryApp());
+}
+
+class FactoryApp extends StatelessWidget {
+  const FactoryApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
